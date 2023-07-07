2e04be1173effe8e5d7e11cd4813a8312c22ceb9
aoito <ao.ito@tabian.co>
2023-07-08 07:54:54 +0900

    🍱 ロゴ追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-
+import LogoImage from 'assets/uww.png'
 </script>
 
 <template>
-  <v-row class="mt-8 justify-center">
-    <common-btn-link
-      text="はじめる"
-      to="/setPlayers"
-      class="align-self-center"
-    />
+  <v-row class="mt-8">
+    <v-col class="text-center">
+      <v-img class="mt-8 mb-8" :src="LogoImage" />
+      <common-btn-link
+        text="はじめる"
+        to="/setPlayers"
+      />
+    </v-col>
   </v-row>
 </template>
