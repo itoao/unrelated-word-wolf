b3cf22b3fea10f8d7f493c5d38556f1c2c2d17b2
aoito <ao.ito@tabian.co>
2023-07-08 02:23:09 +0900

    💄 add common-form-radio

---
+++ src/components/common/field/Radio.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface RadioItem {
+  label: string
+  value: string
+}
+
+type Props = {
+  modelValue?: string
+  title: string
+  radioItems: RadioItem[]
+}
+
+type Emits = {
+  (e: 'update:modelValue', value: string): void
+}
+
+defineProps<Props>()
+
+const emits = defineEmits<Emits>()
+
+const onChange = (value: string) => emits('update:modelValue', value)
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ title }}
+    </p>
+    <v-row>
+      <v-radio-group
+        :model-value="modelValue"
+        inline
+        @update:model-value="onChange"
+      >
+        <v-col
+          v-for="item in radioItems"
+          :key="item.value"
+        >
+          <v-radio
+            :label="item.label"
+            :value="item.value"
+          />
+        </v-col>
+      </v-radio-group>
+    </v-row>
+  </div>
+</template>
