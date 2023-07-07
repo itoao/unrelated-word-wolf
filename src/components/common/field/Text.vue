6cd2da53e7cd0b050466b98c079da7c90e37b5b9
aoito <ao.ito@tabian.co>
2023-07-08 02:23:24 +0900

    💄 add common-field-text

---
+++ src/components/common/field/Text.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+type Props = {
+  clearable?: boolean
+  disabled?: boolean
+  hint?: string
+  label: string
+  modelValue: string
+  placeholder?: string
+  rules?: [(val: string) => true | string]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  clearable: false,
+  disabled: false,
+  hint: '',
+  placeholder: '',
+  rules: undefined
+})
+
+const emit = defineEmits<{(e: 'update:modelValue', value: string): void}>()
+
+const onChange = (event: string) => {
+  emit('update:modelValue', event)
+}
+</script>
+
+<template>
+  <v-text-field
+    v-bind="props"
+    hide-details="auto"
+    persistent-hint
+    @update:model-value="onChange"
+  />
+</template>
