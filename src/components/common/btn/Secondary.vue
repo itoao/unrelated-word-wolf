b739c473fc9fb6a2a6478d09d731b92fce14f6d4
aoito <ao.ito@tabian.co>
2023-07-08 02:22:30 +0900

    💄 add common-btn-secondary

---
+++ src/components/common/btn/Secondary.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean,
+  loading?: boolean,
+  text: string
+}>()
+
+const emits = defineEmits<{(e: 'click'): void}>()
+
+const onClick = () => emits('click')
+</script>
+
+<template>
+  <v-btn
+    color="secondary"
+    :disabled="disabled"
+    :loading="loading"
+    @click="onClick"
+  >
+    {{ text }}
+  </v-btn>
+</template>
