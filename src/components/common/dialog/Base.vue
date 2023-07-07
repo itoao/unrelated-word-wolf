881787ffccb66bbddb8d5654e77b5d36c4b7aa18
aoito <ao.ito@tabian.co>
2023-07-08 02:22:51 +0900

    💄 add common-dialog-base

---
+++ src/components/common/dialog/Base.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+type Props = {
+  modelValue: boolean
+  title: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <v-dialog
+    v-bind="props"
+    max-width="800px"
+    persistent
+    scroll-strategy="none"
+  >
+    <v-card
+      class="pa-4"
+    >
+      <v-card-title
+        class="text-center"
+      >
+        {{ title }}
+      </v-card-title>
+
+      <slot />
+
+      <v-card-actions
+        class="pa-0 mx-auto"
+      >
+        <slot name="footer" />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
