cd98f018f0be5e206352d686237a1099a46a74ed
aoito <ao.ito@tabian.co>
2023-07-08 04:44:47 +0900

    💄 add index.vue

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-row class="mt-8 justify-center">
+    <common-btn-link
+      text="はじめる"
+      to="/setPlayers"
+      class="align-self-center"
+    />
+  </v-row>
+</template>
