f5dabf66573593691317825c8f20a9e8bad997a4
aoito <ao.ito@tabian.co>
2023-07-09 23:47:31 +0900

    🎨 お題表示画面に遷移するようにする

--- src/pages/setGame.vue
+++ src/pages/setGame.vue
@@ -12,4 +12,12 @@ const {
       全員で{{ getPlayer().length }}人
     </v-col>
   </v-row>
+  <v-row class="mt-8">
+    <v-col class="text-center">
+      <common-btn-link
+        text="つぎへ"
+        to="/setQuestion"
+      />
+    </v-col>
+  </v-row>
 </template>
