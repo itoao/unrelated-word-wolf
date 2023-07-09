ff7f2bc49c19dc0b587f5bdc112d8ff3f3adb799
aoito <ao.ito@tabian.co>
2023-07-10 05:08:55 +0900

    🚧 お題画面wip

--- src/pages/setQuestion.vue
+++ src/pages/setQuestion.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
 withHeaderTitle('お題')
+
+const {
+  getGame
+} = useGameStore()
+
+const {
+  time
+} = usePlayingTime()
 </script>
 
 <template>
-  <h1>ワードウルフ</h1>
+  <v-row class="mt-6">
+    <v-col class="text-center">
+      市民: {{ getGame().civilCount }} 人
+    </v-col>
+    <v-col class="text-center">
+      人狼: {{ getGame().wolfCount }} 人
+    </v-col>
+  </v-row>
+
+  <v-row class="mt-6">
+    <v-col class="text-center">
+      話し合い時間: {{ time }} 分
+    </v-col>
+  </v-row>
 </template>
