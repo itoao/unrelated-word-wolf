4a9843e05a0cb1a9ab21a9dd720df20602c9e1d4
aoito <ao.ito@tabian.co>
2023-07-10 02:01:18 +0900

    ✨ レイアウトに戻るボタンを実装

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,11 +2,23 @@
 const {
   title
 } = useHeaderTitle()
+
+const router = useRouter()
+
+const onRouterBack = () => {
+  router.back()
+}
 </script>
 
 <template>
   <v-app>
     <v-app-bar>
+      <common-btn-icon
+        icon="mdi-step-backward"
+        icon-size="x-large"
+        @click="onRouterBack"
+       />
+
       <v-app-bar-title>
         <div class="font-weight-bold">
           {{ title }}
