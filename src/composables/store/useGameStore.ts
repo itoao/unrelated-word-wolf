fea1474dac4e0878cb383a0fe4b2bc85ca1a49c5
aoito <ao.ito@tabian.co>
2023-07-12 23:47:07 +0900

    ♻️ セミコロンを消す

--- src/composables/store/useGameStore.ts
+++ src/composables/store/useGameStore.ts
@@ -7,24 +7,24 @@ type GameInfo = {
  * プレイヤー人数に応じて、市民数・人狼数それぞれの初期値を求める関数
  */
 const initializeCounts = (playerCount: number) => {
-  let wolfCount, civilCount;
+  let wolfCount, civilCount
 
   if (playerCount < 3 || playerCount > 12) {
-    throw new Error('Invalid player count. The number of players must be between 3 and 12.');
+    throw new Error('Invalid player count. The number of players must be between 3 and 12.')
   }
 
   if (playerCount === 3) {
-    wolfCount = 1;
-    civilCount = 2;
+    wolfCount = 1
+    civilCount = 2
   } else if (playerCount % 2 === 0) {
-    wolfCount = Math.floor(playerCount / 4);
-    civilCount = playerCount - wolfCount;
+    wolfCount = Math.floor(playerCount / 4)
+    civilCount = playerCount - wolfCount
   } else {
-    wolfCount = Math.floor(playerCount / 3);
-    civilCount = playerCount - wolfCount;
+    wolfCount = Math.floor(playerCount / 3)
+    civilCount = playerCount - wolfCount
   }
 
-  return { wolfCount, civilCount };
+  return { wolfCount, civilCount }
 }
 
 /**
