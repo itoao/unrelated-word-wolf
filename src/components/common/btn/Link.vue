4360e27b0232d3126f8d7e2ec1b128a7a3cff688
aoito <ao.ito@tabian.co>
2023-07-12 18:14:00 +0900

    🎨 common-btn-linkを、disableのとき遷移させないようにする

--- src/components/common/btn/Link.vue
+++ src/components/common/btn/Link.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import type { AppRoute } from '~/types/AppRoute'
 
-defineProps<{
+withDefaults(defineProps<{
   disabled?: boolean,
   loading?: boolean,
   text: string,
   to: AppRoute
-}>()
+}>(), {
+  disabled: false
+})
 
 const emits = defineEmits<{(e: 'click'): void}>()
 
@@ -14,14 +16,28 @@ const onClick = () => emits('click')
 </script>
 
 <template>
-  <nuxt-link :to="to">
+  <nuxt-link
+    v-if="!disabled"
+    :to="to"
+  >
     <v-btn
       color="primary"
-      :disabled="disabled"
       :loading="loading"
       @click="onClick"
     >
       {{ text }}
     </v-btn>
   </nuxt-link>
+
+  <!-- disabledがtrueだったときにレンダリングするハリボテ -->
+  <template v-else>
+    <v-btn
+      color="primary"
+      disabled
+      :loading="loading"
+      @click="undefined"
+    >
+      {{ text }}
+    </v-btn>
+  </template>
 </template>
